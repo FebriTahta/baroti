c0bdc11c5dc7ebd7678d871ad30911785979a376
FebriTahta <febrirtah@gmail.com>
2022-03-16 22:10:35 +0700

    contact

--- resources/views/fe/contact.blade.php
+++ resources/views/fe/contact.blade.php
@@ -155,8 +155,8 @@
                             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3151.8393924898796!2d144.9536363151022!3d-37.817230742014345!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad65d4e793770d3%3A0x9e44d6ad0d76ba7c!2zMTIxIEtpbmcgU3QsIE1lbGJvdXJuZSBWSUMgMzAwMCwg0JDQstGB0YLRgNCw0LvQuNGP!5e0!3m2!1sru!2sua!4v1469002590349"
                             width="600" height="450"></iframe> --}}
                         {{-- <iframe src="{{$data->map}}" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe> --}}
-                        {!! $contact->map !!}
-
+                        {{-- {!! $contact->map !!} --}}
+                        <iframe src="{{$contact->map}}" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
                     </div>
                 </div>
             </div> <!-- END GOOGLE MAP -->
